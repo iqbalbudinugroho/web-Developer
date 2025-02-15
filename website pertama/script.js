8f01c24acca3a8523f7ab9d28b1b69e2d8b3dcbd
iqbalbudinugroho <iqbalbudinugroho@gmail.com>
2025-02-15 07:42:31 +0000

    project-saya

---
+++ website pertama/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const sections = document.querySelectorAll('section');
+
+    sections.forEach(sections => {
+        sections.addEventListener('click', () => {
+            alert('Anda mengklik bagian: ${section.query')
+        })
+    })
+})
